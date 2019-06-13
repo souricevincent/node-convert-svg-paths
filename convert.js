4f9130743ebb8fd31a7490d296285f3300e97e5d
Vincent Sourice <v.sourice@purjus.fr>
2019-06-13 23:06:12 +0200

    multiple files

--- convert.js
+++ convert.js
@@ -3,45 +3,53 @@ const path = require('path');
 const DOMParser = require('xmldom').DOMParser;
 const svgpath = require('svgpath');
 
-const args = process.argv.slice(2);
+const folderOriginalsSvg = 'originals_svg';
 
-const file = path.join(__dirname, args[0]);
-
-if(!fs.existsSync(file)) {
-    console.log(`${file} not found !`);
+/**
+ * Check if folder "originals_svg" exist
+ */
+if(!fs.existsSync(path.join(__dirname, folderOriginalsSvg))) {
+    console.log(`Create folder "${folderOriginalsSvg}" and put your originals svg !`);
     return;
 }
 
-fs.readFile(file, {encoding: 'utf-8'}, (err, xml) => {
-
-    var doc = new DOMParser().parseFromString(xml, 'text/xml');
-
-    var paths = doc.documentElement.getElementsByTagName('path');
-
-    var resultPaths = [];
-
-    for (var i = 0; i < paths.length; i++) {
-
-        var transformed = new svgpath(paths[i].getAttribute('d'))
-            .unarc()
-            .unshort()
-            .rel()
-            .round(1)
-            .iterate(function(segment, index, x, y) {
-                // Afin de rajouter des courbes sur tous les segments
-                if (segment[0] === 'l') {
-                    // ['courbe', segmentX, segmentY, inflexionX, inflexionY, toX, toY]
-                    segment = ['c', segment[1], segment[2], segment[1], segment[2], segment[1], segment[2]]
-                }
-                return [segment]
-            }).toString()
-            .toString()
-        ;
+var originalsSvgFiles = fs.readdirSync(path.join(__dirname, folderOriginalsSvg))
 
 
-        resultPaths.push(transformed);
+var results = [];
+
+originalsSvgFiles.forEach(file => {
+
+    if(path.extname(file) === '.svg') {
+        var xml = fs.readFileSync(path.join(__dirname, folderOriginalsSvg, file), {encoding: 'utf-8'});
+
+        var doc = new DOMParser().parseFromString(xml, 'text/xml');
+
+        var paths = doc.documentElement.getElementsByTagName('path');
+
+        var arrayPaths = [];
+        for (var i = 0; i < paths.length; i++) {
+            var svgPathTransformed = new svgpath(paths[i].getAttribute('d'))
+                .unarc()
+                .unshort()
+                .rel()
+                .round(1)
+                .iterate(function(segment, index, x, y) {
+                    // Afin de rajouter des courbes sur tous les segments
+                    if (segment[0] === 'l') {
+                        // ['courbe', segmentX, segmentY, inflexionX, inflexionY, toX, toY]
+                        segment = ['c', segment[1], segment[2], segment[1], segment[2], segment[1], segment[2]]
+                    }
+                    return [segment]
+                }).toString()
+                .toString()
+            ;
+            
+            arrayPaths.push(svgPathTransformed);
+        }
+        results.push({[file]: arrayPaths})
     }
-    console.log('d:', resultPaths);
+    
 });
 
-
+console.log(results);
